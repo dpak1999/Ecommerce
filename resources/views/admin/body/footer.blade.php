c0064dfc09d04fa0b9ad7ee70780d96e19324054
dpak1999 <>
2021-05-21 13:39:14 +0530

    refactor admin template

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -5,9 +5,9 @@
                 <a class="nav-link" href="javascript:void(0)">FAQ</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Purchase Now</a>
+                <a class="nav-link" href="#">Website goes here</a>
             </li>
         </ul>
     </div>
-    &copy; 2020 <a href="#">Psd to Html Expert</a>. All Rights Reserved.
+    &copy; 2020 <a href="#">Retsan</a>. All Rights Reserved.
 </footer>
