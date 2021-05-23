97d9aa36bf4123d2fea2e8227fc4959fe3e8e3f1
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-05-23 20:11:19 +0530

    add : brand data table

---
+++ resources/views/backend/brand/brand_view.blade.php
@@ -0,0 +1,47 @@
+@extends('admin.admin_master')
+@section('admin')
+    <div class="container-full">
+        <section class="content">
+            <div class="row">
+                <div class="col-12">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Brand List</h3>
+                        </div>
+                        <div class="box-body">
+                            <div class="table-responsive">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>Brand Name English</th>
+                                            <th>Brand Name Hindi</th>
+                                            <th>Image</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($brands as $item)
+                                            <tr>
+                                                <td>{{ $item->brand_name_en }}</td>
+                                                <td>{{ $item->brand_name_hin }}</td>
+                                                <td>
+                                                    <img src="{{ asset($item->brand_image) }}"
+                                                        style="width: 70px;height: 40px;" alt="...">
+                                                </td>
+                                                <td>
+                                                    <a href="" class="btn btn-info">Edit</a>
+                                                    <a href="" class="btn btn-danger">Delete</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+@endsection
